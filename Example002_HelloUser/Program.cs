02597520dfb316372e20c51cff73b5df37b35b2b
«Sergei» <seregavil@mail.ru>
2022-12-14 17:56:32 +0300

    Добавили гитИгнор и папку Example002_HelloUser

---
+++ Example002_HelloUser/Program.cs
@@ -0,0 +1,5 @@
+﻿//следующей строкой создаем "коробочку" куда попадут данные которые введет пользователь.
+Console.WriteLine("Введите ваше имя");
+string username = Console.ReadLine();
+Console.Write("Привет, ");
+Console.Write(username);
